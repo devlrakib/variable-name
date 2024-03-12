2847d3e3408bbb71f9b036980c4c53756ef58e89
devlrakib <devrakib5@gmail.com>
2024-03-12 18:53:59 +0600

    first commit

---
+++ variable-name.js
@@ -0,0 +1,9 @@
+var price = 20;
+// !Variable Name Can'not be Assigned
+var false = true;
+// ?Variable Name Has to be in one work. No Space
+// ! variable Name Cannot be Quation
+// ?variable Name Cannot Starts With a Number but can ends with a number 
+
+
+
